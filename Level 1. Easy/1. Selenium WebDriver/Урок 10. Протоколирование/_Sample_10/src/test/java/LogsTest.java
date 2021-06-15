78977a7316c2d530b4bd2fb6a346a87f359e8987
diman-the-red-devil <diman_the_red_devil@mail.ru>
2021-06-15 03:10:35 +0300

    Обновил все

---
+++ Level 1. Easy/1. Selenium WebDriver/Урок 10. Протоколирование/_Sample_10/src/test/java/LogsTest.java
@@ -0,0 +1,123 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.Logs;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
+
+public class LogsTest {
+    protected static WebDriver driver;
+    private Logger logger = LogManager.getLogger(LogsTest.class);
+
+    @BeforeEach
+    public void setUp() {
+        driver = WebDriverFactory.getDriver("edge");
+        logger.info("Драйвер стартовал!");
+    }
+
+    @Test
+    public void availableLogTypesTest() {
+        // Получение доступных типов логов
+        Set<String> availableLogTypes = driver.manage().logs().getAvailableLogTypes();
+        for (String s : availableLogTypes)
+            logger.info("Log Type:  " + s);
+    }
+
+    @Test
+    public void logsByTypeTest() {
+        // Открыть страницу https://www.dns-shop.ru/
+        driver.get("https://www.dns-shop.ru/");
+        logger.info("Открыта страница dns-shop.ru - https://www.dns-shop.ru/");
+        // Нажать на ссылку "Да"
+        By linkYesXPath = By.xpath("//a[text()=\"Да\"]");
+        WebElement linkYes = driver.findElement(linkYesXPath);
+        linkYes.click();
+        logger.info("Нажата ссылка \"Да\"");
+        // Нажать на ссылку "Бытовая техника"
+        By linkBTXPath = By.xpath("//div/a[text()=\"Бытовая техника\"]");
+        WebElement linkBT = driver.findElement(linkBTXPath);
+        linkBT.click();
+        logger.info("Нажата ссылка \"Бытовая техника\"");
+        // Получить логи
+        Logs logs = driver.manage().logs();
+        LogEntries logsEntries = logs.get(LogType.BROWSER);
+        List<LogEntry> logsEntriesList = logsEntries.getAll();
+        for (LogEntry logsEntry : logsEntriesList) {
+            logger.info(Date.from(Instant.ofEpochMilli(logsEntry.getTimestamp())) + " " +
+                    logsEntry.getLevel() + " " + logsEntry.getMessage());
+        }
+    }
+
+    @Test
+    public void logsByTypeAndLevelTest() {
+        // Открыть страницу https://www.dns-shop.ru/
+        driver.get("https://www.dns-shop.ru/");
+        logger.info("Открыта страница dns-shop.ru - https://www.dns-shop.ru/");
+        // Нажать на ссылку "Да"
+        By linkYesXPath = By.xpath("//a[text()=\"Да\"]");
+        WebElement linkYes = driver.findElement(linkYesXPath);
+        linkYes.click();
+        logger.info("Нажата ссылка \"Да\"");
+        // Нажать на ссылку "Бытовая техника"
+        By linkBTXPath = By.xpath("//div/a[text()=\"Бытовая техника\"]");
+        WebElement linkBT = driver.findElement(linkBTXPath);
+        linkBT.click();
+        logger.info("Нажата ссылка \"Бытовая техника\"");
+        // Получить логи
+        Logs logs = driver.manage().logs();
+        LogEntries logsEntries = logs.get(LogType.BROWSER);
+        List<LogEntry> logsEntriesList = logsEntries.getAll().stream()
+                .filter(a -> a.getLevel() == Level.SEVERE)
+                .collect(Collectors.toList());
+        for (LogEntry logsEntry : logsEntriesList) {
+            logger.info(Date.from(Instant.ofEpochMilli(logsEntry.getTimestamp())) + " " +
+                    logsEntry.getLevel() + " " + logsEntry.getMessage());
+        }
+    }
+
+    @Test
+    public void logsConfigurationTest() {
+        // Открыть страницу https://www.dns-shop.ru/
+        driver.get("https://www.dns-shop.ru/");
+        logger.info("Открыта страница dns-shop.ru - https://www.dns-shop.ru/");
+        // Нажать на ссылку "Да"
+        By linkYesXPath = By.xpath("//a[text()=\"Да\"]");
+        WebElement linkYes = driver.findElement(linkYesXPath);
+        linkYes.click();
+        logger.info("Нажата ссылка \"Да\"");
+        // Нажать на ссылку "Бытовая техника"
+        By linkBTXPath = By.xpath("//div/a[text()=\"Бытовая техника\"]");
+        WebElement linkBT = driver.findElement(linkBTXPath);
+        linkBT.click();
+        logger.info("Нажата ссылка \"Бытовая техника\"");
+        // Получить логи
+        Logs logs = driver.manage().logs();
+        LogEntries logsEntries = logs.get(LogType.BROWSER);
+        List<LogEntry> logsEntriesList = logsEntries.getAll();
+        for (LogEntry logsEntry : logsEntriesList) {
+            logger.info(Date.from(Instant.ofEpochMilli(logsEntry.getTimestamp())) + " " +
+                    logsEntry.getLevel() + " " + logsEntry.getMessage());
+        }
+    }
+
+    @AfterEach
+    public void setDown() {
+        if(driver != null) {
+            driver.quit();
+            logger.info("Драйвер остановлен!");
+        }
+    }
+}
---
+++ Level 1. Easy/1. Selenium WebDriver/Урок 10. Протоколирование/_Sample_10/src/test/java/WebDriverFactory.java
@@ -0,0 +1,70 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.logging.Level;
+
+public class WebDriverFactory {
+    private static Logger logger = LogManager.getLogger(WebDriverFactory.class);
+
+    public static WebDriver getDriver(String browserName) {
+
+        switch (browserName) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                logger.info("Драйвер для браузера Google Chrome");
+                /*
+                LoggingPreferences chromeLogPrefs = new LoggingPreferences();
+                chromeLogPrefs.enable(LogType.BROWSER, Level.ALL);
+                chromeLogPrefs.enable(LogType.CLIENT, Level.ALL);
+                chromeLogPrefs.enable(LogType.DRIVER, Level.ALL);
+                chromeLogPrefs.enable(LogType.PERFORMANCE, Level.ALL);
+                chromeLogPrefs.enable(LogType.SERVER, Level.ALL);
+                DesiredCapabilities chromeCaps = new DesiredCapabilities();
+                chromeCaps.setCapability(CapabilityType.LOGGING_PREFS, chromeLogPrefs);
+                */
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--start-maximized");
+                // chromeOptions.merge(chromeCaps);
+                return new ChromeDriver(chromeOptions);
+            case "firefox" :
+                WebDriverManager.firefoxdriver().setup();
+                logger.info("Драйвер для браузера Mozilla Firefox");
+                return new FirefoxDriver();
+            case "edge" :
+                WebDriverManager.edgedriver().setup();
+                logger.info("Драйвер для браузера Microsoft Edge");
+                LoggingPreferences edgeLogPrefs = new LoggingPreferences();
+                edgeLogPrefs.enable(LogType.BROWSER, Level.SEVERE);
+                DesiredCapabilities edgeCaps = new DesiredCapabilities();
+                edgeCaps.setCapability(CapabilityType.LOGGING_PREFS, edgeLogPrefs);
+
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--start-maximized");
+                edgeOptions.merge(edgeCaps);
+                return new EdgeDriver(edgeOptions);
+            case "explorer" :
+                WebDriverManager.iedriver().setup();
+                logger.info("Драйвер для браузера Microsoft Internet Explorer");
+                return new InternetExplorerDriver();
+            case "opera" :
+                WebDriverManager.operadriver().setup();
+                logger.info("Драйвер для браузера Opera");
+                return new OperaDriver();
+            default:
+                throw new RuntimeException("Incorrect browser name");
+        }
+    }
+}
