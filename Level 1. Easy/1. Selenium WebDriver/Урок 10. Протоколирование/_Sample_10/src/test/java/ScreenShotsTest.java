b794ebb935b6f6dc3f15d6dd3f60c72b92fb6456
diman_the_red_devil <diman_the_red_devil@mail.ru>
2021-06-21 02:04:45 +0300

    Обновил все

--- Level 1. Easy/1. Selenium WebDriver/Урок 10. Протоколирование/_Sample_10/src/test/java/ScreenShotsTest.java
+++ Level 1. Easy/1. Selenium WebDriver/Урок 10. Протоколирование/_Sample_10/src/test/java/ScreenShotsTest.java
@@ -1,16 +1,22 @@
-import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.comparison.ImageDiff;
+import ru.yandex.qatools.ashot.comparison.ImageDiffer;
+import ru.yandex.qatools.ashot.coordinates.WebDriverCoordsProvider;
+import ru.yandex.qatools.ashot.cropper.indent.BlurFilter;
+import ru.yandex.qatools.ashot.cropper.indent.IndentCropper;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Set;
 
 public class ScreenShotsTest {
     protected static WebDriver driver;
@@ -37,11 +43,12 @@ public class ScreenShotsTest {
         WebElement linkBT = driver.findElement(linkBTXPath);
         linkBT.click();
         logger.info("Нажата ссылка \"Бытовая техника\"");
-        // Сделать скриншот всей страницы
-        File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        // Сделать скриншот видимой области веб страницы
         try {
-            FileUtils.copyFile(file, new File("temp\\screenshot.png"));
-            logger.info("Скриншот сохранен в файле [temp\\screenshot.png]");
+            File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            BufferedImage image = ImageIO.read(file);
+            ImageIO.write(image, "png", new File("temp\\TSViewablePage.png"));
+            logger.info("Скриншот сохранен в файле [temp\\TSViewablePage.png]");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -52,13 +59,24 @@ public class ScreenShotsTest {
         // Открыть страницу https://www.dns-shop.ru/
         driver.get("https://www.dns-shop.ru/");
         logger.info("Открыта страница dns-shop.ru - https://www.dns-shop.ru/");
-        // Сделать скриншот ссылки "Да"
+        // Нажать на ссылку "Да"
         By linkYesXPath = By.xpath("//a[text()=\"Да\"]");
         WebElement linkYes = driver.findElement(linkYesXPath);
-        File file = linkYes.getScreenshotAs(OutputType.FILE);
+        linkYes.click();
+        logger.info("Нажата ссылка \"Да\"");
+        // Нажать на ссылку "Бытовая техника"
+        By linkBTXPath = By.xpath("//div/a[text()=\"Бытовая техника\"]");
+        WebElement linkBT = driver.findElement(linkBTXPath);
+        linkBT.click();
+        logger.info("Нажата ссылка \"Бытовая техника\"");
+        // Сделать скриншот веб элемента
         try {
-            FileUtils.copyFile(file, new File("temp\\screenshot.png"));
-            logger.info("Скриншот сохранен в файле [temp\\screenshot.png]");
+            By divSubcategoryXPath = By.xpath("//div[@class=\"subcategory\"]");
+            WebElement divSubcategory = driver.findElement(divSubcategoryXPath);
+            File file = divSubcategory.getScreenshotAs(OutputType.FILE);
+            BufferedImage viewablePageImage = ImageIO.read(file);
+            ImageIO.write(viewablePageImage, "png", new File("temp\\TSElement.png"));
+            logger.info("Скриншот сохранен в файле [temp\\TSElement.png]");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -69,23 +87,171 @@ public class ScreenShotsTest {
         // Открыть страницу https://www.dns-shop.ru/
         driver.get("https://www.dns-shop.ru/");
         logger.info("Открыта страница dns-shop.ru - https://www.dns-shop.ru/");
-        // Найти элемент
+        // Нажать на ссылку "Да"
         By linkYesXPath = By.xpath("//a[text()=\"Да\"]");
         WebElement linkYes = driver.findElement(linkYesXPath);
-        // Получить координаты и размеры элемента
-        Point location = linkYes.getLocation();
-        Dimension size = linkYes.getSize();
-        // Сделать скриншот всей страницы
-        File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        // Сделать скриншот ссылки "Да"
+        linkYes.click();
+        logger.info("Нажата ссылка \"Да\"");
+        // Нажать на ссылку "Бытовая техника"
+        By linkBTXPath = By.xpath("//div/a[text()=\"Бытовая техника\"]");
+        WebElement linkBT = driver.findElement(linkBTXPath);
+        linkBT.click();
+        logger.info("Нажата ссылка \"Бытовая техника\"");
+        // Сделать скриншот веб элемента вырезанный из видимой области веб страницы
         try {
+            By divSubcategoryXPath = By.xpath("//div[@class=\"subcategory\"]");
+            WebElement divSubcategory = driver.findElement(divSubcategoryXPath);
+            Point location = divSubcategory.getLocation();
+            Dimension size = divSubcategory.getSize();
+            File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
             BufferedImage viewablePageImage = ImageIO.read(file);
             BufferedImage linkYesImage = viewablePageImage.getSubimage(
                 location.getX() - 50, location.getY() - 50,
                 size.getWidth() + 200, size.getHeight() + 100);
-            ImageIO.write(linkYesImage, "png", file);
-            FileUtils.copyFile(file, new File("temp\\screenshot.png"));
-            logger.info("Скриншот сохранен в файле [temp\\screenshot.png]");
+            ImageIO.write(linkYesImage, "png", new File("temp\\TSElementFromViewablePage.png"));
+            logger.info("Скриншот сохранен в файле [temp\\TSElementFromViewablePage.png]");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void aShotViewablePageTest() {
+        // Открыть страницу https://www.dns-shop.ru/
+        driver.get("https://www.dns-shop.ru/");
+        logger.info("Открыта страница dns-shop.ru - https://www.dns-shop.ru/");
+        // Нажать на ссылку "Да"
+        By linkYesXPath = By.xpath("//a[text()=\"Да\"]");
+        WebElement linkYes = driver.findElement(linkYesXPath);
+        linkYes.click();
+        logger.info("Нажата ссылка \"Да\"");
+        // Нажать на ссылку "Бытовая техника"
+        By linkBTXPath = By.xpath("//div/a[text()=\"Бытовая техника\"]");
+        WebElement linkBT = driver.findElement(linkBTXPath);
+        linkBT.click();
+        logger.info("Нажата ссылка \"Бытовая техника\"");
+        // Сделать скриншот видимой области веб страницы
+        try {
+            Screenshot screenshot = new AShot().takeScreenshot(driver);
+            ImageIO.write(screenshot.getImage(), "png", new File("temp\\ASViewablePage.png"));
+            logger.info("Скриншот сохранен в файле [temp\\ASViewablePage.png]");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void aShotFullPageTest() {
+        // Открыть страницу https://www.dns-shop.ru/
+        driver.get("https://www.dns-shop.ru/");
+        logger.info("Открыта страница dns-shop.ru - https://www.dns-shop.ru/");
+        // Нажать на ссылку "Да"
+        By linkYesXPath = By.xpath("//a[text()=\"Да\"]");
+        WebElement linkYes = driver.findElement(linkYesXPath);
+        linkYes.click();
+        logger.info("Нажата ссылка \"Да\"");
+        // Нажать на ссылку "Бытовая техника"
+        By linkBTXPath = By.xpath("//div/a[text()=\"Бытовая техника\"]");
+        WebElement linkBT = driver.findElement(linkBTXPath);
+        linkBT.click();
+        logger.info("Нажата ссылка \"Бытовая техника\"");
+        // Сделать скриншот всей веб страницы
+        try {
+            Screenshot screenshot = new AShot()
+                    .shootingStrategy(ShootingStrategies.viewportPasting(100))
+                    .takeScreenshot(driver);
+            ImageIO.write(screenshot.getImage(), "png", new File("temp\\ASFullPage.png"));
+            logger.info("Скриншот сохранен в файле [temp\\ASFullPage.png]");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void aShotElementTest() {
+        // Открыть страницу https://www.dns-shop.ru/
+        driver.get("https://www.dns-shop.ru/");
+        logger.info("Открыта страница dns-shop.ru - https://www.dns-shop.ru/");
+        // Нажать на ссылку "Да"
+        By linkYesXPath = By.xpath("//a[text()=\"Да\"]");
+        WebElement linkYes = driver.findElement(linkYesXPath);
+        linkYes.click();
+        logger.info("Нажата ссылка \"Да\"");
+        // Нажать на ссылку "Бытовая техника"
+        By linkBTXPath = By.xpath("//div/a[text()=\"Бытовая техника\"]");
+        WebElement linkBT = driver.findElement(linkBTXPath);
+        linkBT.click();
+        logger.info("Нажата ссылка \"Бытовая техника\"");
+        // Сделать скриншот веб элемента
+        try {
+            By divSubcategoryXPath = By.xpath("//div[@class=\"subcategory\"]");
+            WebElement divSubcategory = driver.findElement(divSubcategoryXPath);
+            Screenshot screenshot = new AShot()
+                    .coordsProvider(new WebDriverCoordsProvider())
+                    .takeScreenshot(driver, divSubcategory);
+            ImageIO.write(screenshot.getImage(), "png", new File("temp\\ASElement.png"));
+            logger.info("Скриншот сохранен в файле [temp\\ASElement.png]");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void aShotCompareImagesTest() {
+        // Открыть страницу https://www.dns-shop.ru/
+        driver.get("https://www.dns-shop.ru/");
+        logger.info("Открыта страница dns-shop.ru - https://www.dns-shop.ru/");
+        // Нажать на ссылку "Да"
+        By linkYesXPath = By.xpath("//a[text()=\"Да\"]");
+        WebElement linkYes = driver.findElement(linkYesXPath);
+        linkYes.click();
+        logger.info("Нажата ссылка \"Да\"");
+        // Нажать на ссылку "Бытовая техника"
+        By linkBTXPath = By.xpath("//div/a[text()=\"Бытовая техника\"]");
+        WebElement linkBT = driver.findElement(linkBTXPath);
+        linkBT.click();
+        logger.info("Нажата ссылка \"Бытовая техника\"");
+        // Сравнить скриншот с образцом
+        try {
+            BufferedImage actualImage = new AShot().takeScreenshot(driver).getImage();
+            BufferedImage expectedImage = ImageIO.read(new File("temp\\ASCompareImagesExpectedImage.png"));
+            ImageDiffer imageDiffer = new ImageDiffer();
+            ImageDiff diff = imageDiffer.makeDiff(actualImage, expectedImage);
+            if (diff.hasDiff()) {
+                logger.info("Изображения одинаковые");
+            } else {
+                logger.info("Изображения разные");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void aShotPrettifyTest() {
+        // Открыть страницу https://www.dns-shop.ru/
+        driver.get("https://www.dns-shop.ru/");
+        logger.info("Открыта страница dns-shop.ru - https://www.dns-shop.ru/");
+        // Нажать на ссылку "Да"
+        By linkYesXPath = By.xpath("//a[text()=\"Да\"]");
+        WebElement linkYes = driver.findElement(linkYesXPath);
+        linkYes.click();
+        logger.info("Нажата ссылка \"Да\"");
+        // Нажать на ссылку "Бытовая техника"
+        By linkBTXPath = By.xpath("//div/a[text()=\"Бытовая техника\"]");
+        WebElement linkBT = driver.findElement(linkBTXPath);
+        linkBT.click();
+        logger.info("Нажата ссылка \"Бытовая техника\"");
+        // Сделать скриншот с эффектом
+        try {
+            By divSubcategoryXPath = By.xpath("//div[@class=\"subcategory\"]");
+            WebElement divSubcategory = driver.findElement(divSubcategoryXPath);
+            Screenshot screenshot = new AShot()
+                    .imageCropper(new IndentCropper()
+                            .addIndentFilter(new BlurFilter()))
+                    .takeScreenshot(driver, divSubcategory);
+            ImageIO.write(screenshot.getImage(), "png", new File("temp\\ASPrettify.png"));
+            logger.info("Скриншот сохранен в файле [temp\\ASPrettify.png]");
         } catch (IOException e) {
             e.printStackTrace();
         }
