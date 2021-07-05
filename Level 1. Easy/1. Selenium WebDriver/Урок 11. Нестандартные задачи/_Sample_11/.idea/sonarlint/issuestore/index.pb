a72d15fbc76409da472a8e4b27774a391b473426
U_M11W2 <dskim@alfabank.ru>
2021-07-05 18:03:54 +0300

    Обновил все

---
+++ Level 1. Easy/1. Selenium WebDriver/Урок 11. Нестандартные задачи/_Sample_11/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+M
+src/test/java/SampleTest.java,6\2\62e317d220e9a32c375e26eadd28d45d36e7934a
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ Level 1. Easy/1. Selenium WebDriver/Урок 11. Нестандартные задачи/_Sample_11/src/test/java/SampleTest.java
@@ -0,0 +1,78 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class SampleTest {
+    protected static WebDriver driver;
+    private Logger logger = LogManager.getLogger(SampleTest.class);
+
+    // Читаем передаваемый параметр browser (-Dbrowser)
+    String env = System.getProperty("browser", "chrome");
+
+    /*
+    Варианты команд для запуска
+
+    mvn clean test -Dbrowser='cHrOmE'
+    mvn clean test -Dbrowser=cHrOmE
+    mvn clean test -Dbrowser='chrome'
+    mvn clean test -Dbrowser=chrome
+
+    mvn clean test -Dbrowser='FiReFoX'
+    mvn clean test -Dbrowser=FiReFoX
+    mvn clean test -Dbrowser='firefox'
+    mvn clean test -Dbrowser=firefox
+     */
+
+    @BeforeEach
+    public void setUp() {
+        logger.info("env = " + env);
+        driver = WebDriverFactory.getDriver(env.toLowerCase());
+        logger.info("Драйвер стартовал!");
+    }
+
+    @Test
+    public void openPage() {
+        driver.get("https://yandex.ru/");
+        logger.info("Открыта страница Yandex - " + "https://yandex.ru/");
+
+        // Вывод заголовка страницы
+        String title = driver.getTitle();
+        logger.info("title - " + title.toString());
+
+        // Вывод текущего URL
+        String currentUrl = driver.getCurrentUrl();
+        logger.info("current URL - " + currentUrl.toString());
+
+        // Ввод текста для поиска
+        String searchInputXpath = ".//input[@class='input__control input__input mini-suggest__input']";
+        WebElement searchInput = driver.findElement(By.xpath(searchInputXpath));
+        String searchText = "Java";
+        searchInput.sendKeys(searchText);
+
+        // Нажатие кнопка "Найти"
+        String searchButtonXpath = ".//button[@class='button mini-suggest__button button_theme_search button_size_search i-bem button_js_inited']";
+        WebElement searchButton = driver.findElement(By.xpath(searchButtonXpath));
+        searchButton.click();
+
+        // Добавляем задержку sleep чтобы увидеть результат
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterEach
+    public void setDown() {
+        if(driver != null) {
+            driver.quit();
+            logger.info("Драйвер остановлен!");
+        }
+    }
+}
+
