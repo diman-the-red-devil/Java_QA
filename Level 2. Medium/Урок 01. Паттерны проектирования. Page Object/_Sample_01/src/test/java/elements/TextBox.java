afda733a44a64e4e0e792cca1512228ddad42452
diman_the_red_devil <diman_the_red_devil@mail.ru>
2021-08-12 23:55:14 +0300

    Обновил все

--- Level 2. Medium/Урок 01. Паттерны проектирования. Page Object/_Sample_01/src/test/java/elements/TextBox.java
+++ Level 2. Medium/Урок 01. Паттерны проектирования. Page Object/_Sample_01/src/test/java/elements/TextBox.java
@@ -2,16 +2,20 @@ package elements;
 
 import org.openqa.selenium.WebElement;
 
+// Класс Текстовое Поле (TextBox)
 public class TextBox extends Element {
+    // Конструктор
     public TextBox(WebElement webElement) {
         super(webElement);
     }
 
+    // Нажатие на текстовое поле
     public void click() {
         webElement.click();
     }
 
-    public void setText(String text) {
+    // Ввод текста в текстовое поле
+    public void setValue(String text) {
         webElement.sendKeys(text);
     }
 }
