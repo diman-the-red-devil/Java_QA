708ec12bdcc2746717f0773dceb3f4f5d55c9694
Red Devil <diman_the_red_devil@mail.ru>
2022-11-29 23:52:07 +0300

    Обновил все!

--- 14. API. REST Assured/__/src/test/java/Sample.java
+++ 14. API. REST Assured/__/src/test/java/Sample.java
@@ -2,20 +2,45 @@ import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class Sample {
 
+    @Test
+    public void restTest1() {
+        String jsonString = "{\"username\" : \"admin\",\"password\" : \"password123\"}";
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("https://restful-booker.herokuapp.com")
+                .setContentType(ContentType.JSON)
+                .setBody(jsonString)
+                .build();
+
+        Response response = RestAssured.post("/auth");
+        response.prettyPrint();
+    }
 
     @Test
-    public void restTest() {
-        RestAssured.requestSpecification = new RequestSpecBuilder()
-                .setBaseUri("https://restful-booker.herokuapp.com/auth")
-                .setBody("{\"username\" : \"admin\",\"password\" : \"password123\"}")
-                .setContentType(ContentType.JSON).build();
-        Response response = RestAssured.post();
-        response.prettyPrint();
+    public void restTest2() {
+        String jsonString = "{\r\n" +
+                "    \"firstname\" : \"Amod\",\r\n" +
+                "    \"lastname\" : \"Mahajan\"}";
+
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com/booking/1")
+                .cookie("token", "6608dc75eedd44f")
+                .contentType(ContentType.JSON)
+                .body(jsonString)
+                .when()
+                .patch()
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("firstname", Matchers.equalTo("Amod"))
+                .body("lastname", Matchers.equalTo("Mahajan"));
     }
 }
