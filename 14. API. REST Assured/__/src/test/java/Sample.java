5be70ab926ebe85d1f6be26a8fa9aaa20cf92950
Ким Дмитрий Сергеевич <DSKim@alfabank.ru>
2022-12-01 18:29:37 +0300

    Обновил все

--- 14. API. REST Assured/__/src/test/java/Sample.java
+++ 14. API. REST Assured/__/src/test/java/Sample.java
@@ -4,6 +4,7 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -12,35 +13,29 @@ public class Sample {
 
     @Test
     public void restTest1() {
-        String jsonString = "{\"username\" : \"admin\",\"password\" : \"password123\"}";
-        RestAssured.requestSpecification = new RequestSpecBuilder()
-                .setBaseUri("https://restful-booker.herokuapp.com")
-                .setContentType(ContentType.JSON)
-                .setBody(jsonString)
-                .build();
+        RequestSpecification request = RestAssured.given();
+        request.baseUri("https://rickandmortyapi.com/api/");
+        request.basePath("/character");
+        request.accept(ContentType.JSON);
+        request.queryParam("name","Rick Sanchez");
 
-        Response response = RestAssured.post("/auth");
-        response.prettyPrint();
+        Response response = request.request("GET");
+        long timeIn  = response.timeIn(TimeUnit.MILLISECONDS);
+        System.out.println("timeIn: " + timeIn);
     }
 
     @Test
     public void restTest2() {
-        String jsonString = "{\r\n" +
-                "    \"firstname\" : \"Amod\",\r\n" +
-                "    \"lastname\" : \"Mahajan\"}";
+        RequestSpecification rqstSpec = new RequestSpecBuilder()
+                .setBaseUri()
+                .setBasePath();
+        rqstSpec.accept(ContentType.JSON);
+        rqstSpec.queryParam("name","Rick Sanchez")
+                .log().method()
+                .log().params();
 
-        RestAssured
-                .given()
-                .baseUri("https://restful-booker.herokuapp.com/booking/1")
-                .cookie("token", "6608dc75eedd44f")
-                .contentType(ContentType.JSON)
-                .body(jsonString)
-                .when()
-                .patch()
-                .then()
-                .assertThat()
-                .statusCode(200)
-                .body("firstname", Matchers.equalTo("Amod"))
-                .body("lastname", Matchers.equalTo("Mahajan"));
+        ResponseSpecification rspsSpec = rqstSpec.response();
+        rspsSpec.statusCode(200);
+        rspsSpec.request().request("GET","/character");
     }
 }
