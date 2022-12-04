7d0069bb681d2a2f83cf7ea6304cd2c4eb96cc76
Red Devil <diman_the_red_devil@mail.ru>
2022-12-04 14:15:00 +0300

    Обновил все!

--- 14. API. REST Assured/__/src/test/java/Sample.java
+++ 14. API. REST Assured/__/src/test/java/Sample.java
@@ -1,7 +1,10 @@
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
@@ -13,10 +16,12 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
+import static org.hamcrest.Matchers.equalTo;
+
 public class Sample {
 
     @Test
-    public void test1() {
+    public void test0() {
         RequestSpecification request = RestAssured.given();
         request.baseUri("https://rickandmortyapi.com/api");
         request.basePath("/character");
@@ -24,17 +29,32 @@ public class Sample {
 
         Response response = request.get();
         response.prettyPrint();
+    }
+
+    @Test
+    public void test1() {
+        ResponseSpecification response = RestAssured.expect();
+        response.statusCode(500);
+
+        RequestSpecification request = RestAssured.given();
+        request.baseUri("https://rickandmortyapi.com/api");
+        request.basePath("/character");
+        request.queryParam("name","Rick Sanchez");
+
+        request.get().then().spec(response);
     }
 
     @Test
     public void test2() {
+        ResponseSpecification response = RestAssured.expect();
+        response.
+        response.statusCode(500);
+
         RequestSpecification request = RestAssured.given();
         request.baseUri("https://rickandmortyapi.com/api");
         request.basePath("/character");
         request.queryParam("name","Rick Sanchez");
 
-        Response response = request.get();
-        response.prettyPrint();
+        request.get().then().spec(response);
     }
-
 }
